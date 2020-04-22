d6cdb12ef5590aa669aa5dbe894da8764c9769f6
Kaung Khant Kyaw <58139120+kaungkhantjc@users.noreply.github.com>
2020-04-22 21:25:34 +0630

    fixed DatePickerDialog crash in API 23 and under
    public DatePickerDialog (Context context)
    
    this constructor is only available in API 24 and above.

--- app/src/main/java/com/jcsamples/exchangerates/MainActivity.java
+++ app/src/main/java/com/jcsamples/exchangerates/MainActivity.java
@@ -127,8 +127,10 @@ public class MainActivity extends Activity
 
 	private void showDatePickerDialog(){
 		DatePickerDialog dialog = null;
-		if (pickedDates == null) dialog = new DatePickerDialog(this);
-		else dialog = new DatePickerDialog (this, null, pickedDates[0], pickedDates[1], pickedDates[2]);
+		if (pickedDates == null) {
+			Calendar cal = Calendar.getInstance();
+			dialog = new DatePickerDialog(this, null, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+		} else dialog = new DatePickerDialog (this, null, pickedDates[0], pickedDates[1], pickedDates[2]);
 		
 		dialog.getDatePicker().setMaxDate(System.currentTimeMillis());
 		dialog.setOnDateSetListener(new DatePickerDialog.OnDateSetListener(){
